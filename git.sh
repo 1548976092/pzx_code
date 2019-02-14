d7aae063b1a62abe79ed2c81db8383d0d32286d8
1548976092 <1548976092@qq.com>
2019-02-13 19:41:12 -0800

    push.sh

--- git.sh
+++ git.sh
@@ -1,4 +1,5 @@
 #初始化仓库
+git config credential.helper store
 git config --global user.name "1548976092" 
 git config --global user.email "1548976092@qq.com"
 git init
@@ -11,5 +12,5 @@ git log
 git status
 
 #推送远程仓库
-git remote add origin https://github.com/1548976092/pzx_code.git
+git remote add origin https://github.com/1548976092/git_go.git
 git push -u origin master
