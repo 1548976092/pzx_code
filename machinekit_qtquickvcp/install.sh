dc03072e12c307c8cfc07fc194c2672b2fd81ad8
1548976092 <1548976092@qq.com>
2019-06-26 19:18:35 +0800

    push.sh

--- machinekit_qtquickvcp/install.sh
+++ machinekit_qtquickvcp/install.sh
@@ -1,5 +1,6 @@
 sudo apt install qml-module-qttest  QtQuick.XmlListModel QtQuick.Dialogs QtQuick.Particles 
 sudo apt install QtQuick.XmlListModel QtQuick.Dialogs QtQuick.Particles QtQuick.XmlListModel 
 sudo apt install QtQuick.Dialogs QtQuick.Particles Qt.Labs.settings 
+//以下使用qml的designer很关键
 sudo apt install Qt.labs.folderlistmodel
 sudo apt install QtQuick.Extras
