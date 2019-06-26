a140ce118812c89c8b5f49c3d96d11d2fd7bf03c
1548976092 <1548976092@qq.com>
2019-06-26 19:16:13 +0800

    readme.txt

---
+++ machinekit_qtquickvcp/install.sh
@@ -0,0 +1,5 @@
+sudo apt install qml-module-qttest  QtQuick.XmlListModel QtQuick.Dialogs QtQuick.Particles 
+sudo apt install QtQuick.XmlListModel QtQuick.Dialogs QtQuick.Particles QtQuick.XmlListModel 
+sudo apt install QtQuick.Dialogs QtQuick.Particles Qt.Labs.settings 
+sudo apt install Qt.labs.folderlistmodel
+sudo apt install QtQuick.Extras
