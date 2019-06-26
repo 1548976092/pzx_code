ac5f3077077fd61bf662f80fdaeafb9118bf7cba
1548976092 <1548976092@qq.com>
2019-06-26 19:23:22 +0800

    push.sh

--- machinekit_qtquickvcp/install.sh
+++ machinekit_qtquickvcp/install.sh
@@ -1,6 +1,19 @@
+//先安装qt5
+sudo apt-get install build-essential
+sudo apt-get install qtcreator
+sudo apt-get install qt5-default
+
+sudo apt-get install qtdeclarative5-dev qml-module-qtquick-controls
+
+//克隆库
+git clone https://github.com/machinekit/QtQuickVcp.git
+//安装插件
 sudo apt install qml-module-qttest  QtQuick.XmlListModel QtQuick.Dialogs QtQuick.Particles 
 sudo apt install QtQuick.XmlListModel QtQuick.Dialogs QtQuick.Particles QtQuick.XmlListModel 
 sudo apt install QtQuick.Dialogs QtQuick.Particles Qt.Labs.settings 
 //以下使用qml的designer很关键
 sudo apt install Qt.labs.folderlistmodel
 sudo apt install QtQuick.Extras
+
+
+//在realese里面 make install,docs,docs install
