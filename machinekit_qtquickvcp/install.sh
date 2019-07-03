75f5681441e2ec4010248df39e9b50c98c1cf2f6
1548976092 <1548976092@qq.com>
2019-07-03 11:45:49 +0800

    push.sh

--- machinekit_qtquickvcp/install.sh
+++ machinekit_qtquickvcp/install.sh
@@ -6,16 +6,16 @@ sudo apt-get install qt5-default
 sudo apt-get install qtdeclarative5-dev qml-module-qtquick-controls
 
 //克隆库
-git clone https://github.com/machinekit/QtQuickVcp.git
+//git clone https://github.com/machinekit/QtQuickVcp.git
 //安装插件
 sudo apt install qml-module-qttest  QtQuick.XmlListModel QtQuick.Dialogs QtQuick.Particles 
 sudo apt install QtQuick.XmlListModel QtQuick.Dialogs QtQuick.Particles QtQuick.XmlListModel 
 sudo apt install QtQuick.Dialogs QtQuick.Particles Qt.Labs.settings 
 
 //配置qmake
-QMAKE_DEFAULT_INCDIRS=\\
-或
-QMAKE_CFLAGS_ISYSTEM=-I
+//QMAKE_DEFAULT_INCDIRS=\\
+//或
+//QMAKE_CFLAGS_ISYSTEM=-I
 
 //以下使用qml的designer很关键
 sudo apt install Qt.labs.folderlistmodel
